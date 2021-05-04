2b584b487c2c9a080034742055510fe38336dac3
zwiebelLiu <qin.liu1989@yahoo.de>
2021-05-04 14:54:01 +0200

    validated() ->storeRequest  && validate() ->Request diff

--- app/Http/Requests/StoreChecklistGroupRequest.php
+++ app/Http/Requests/StoreChecklistGroupRequest.php
@@ -27,4 +27,21 @@ class StoreChecklistGroupRequest extends FormRequest
             'name'=>['required','unique:checklist_groups'],
         ];
     }
+
+    /**
+     * 配置验证器实例.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+                $validator->errors()->add('field', 'Something is wrong with this field!');
+
+        });
+    }
 }
+
+
